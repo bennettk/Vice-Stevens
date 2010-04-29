978ba1f7648c67f49bfd2af519bb2c478029daec
Bennett Kolasinski <bennettk@bennettk.local>
2010-04-29 15:34:33 -0400

    facebook, twits

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+function get_latest_tweet($username)
+{
+	$path = 'http://twitter.com/statuses/user_timeline/' . $username.'.json?count=1';
+	$jason = file_get_contents($path);
+	$arr = json_decode($jason);
+	$object = get_object_vars($arr[0]);
+	return $object['text'];
+}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>VICE STEVENS</title>
+
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css">
+
+<style type="text/css">
+	body { background-color: #00a1f2; min-height: 800px;}
+	#vicetext { position: absolute; top: 0px; left: 0px; width: 700px; height: 700px; background-image: url(vicestevens.png);}
+	#social { position:absolute; top: 250px; left: 450px; width: 500px;}
+	#social img { opacity: .6; }
+	#social img:hover { opacity: 1.0; }
+	#status { color: #fff; font-family: "Verdana"; font-size: 3em; letter-spacing: -.1em; }
+</style>
+
+</head>
+<body>
+	<div id="vicetext">
+	
+	</div>
+	
+
+	
+	<div id="social">
+		<div id="status">
+			<h1><?= get_latest_tweet('vicestevens'); ?></h1>
+		</div>
+		<a href="http://amiestreet.com/music/vice-stevens/">
+			<img src="amie.png" alt="hear us on amie street" />
+		</a>
+		<a href="http://www.facebook.com/pages/Vice-Stevens/6689799494">
+			<img src="facebook.png" alt="like us on facebook" />
+		</a>
+		<a href="http://www.youtube.com/user/vicestevens">
+			<img src="youtube.png" alt="look at us on youtube" />
+		</a>
+		<iframe src="http://www.facebook.com/plugins/like.php?href=www.vicestevens.com&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;font=verdana&amp;colorscheme=blue" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:450px; height:px"></iframe>
+	</div>
+</body>
+</html>
